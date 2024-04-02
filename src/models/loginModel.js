0b36cce29039060d816d6b65f53806070872709e
Fillip14 <fillip.apol@gmail.com>
2024-04-02 16:30:01 -0300

    Adicionado tratamento para esconder senha no db e verificar se ja existe email criado

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -1,5 +1,6 @@
 const moongose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new moongose.Schema({
   email: { type: String, required: true },
@@ -17,14 +18,25 @@ class Login {
 
   async register() {
     this.valida();
+    if (this.errors.length > 0) return;
+
+    await this.userExists();
 
     if (this.errors.length > 0) return;
+
+    const salt = bcryptjs.genSaltSync();
+    this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
     try {
       this.user = await LoginModel.create(this.body);
     } catch (e) {
       console.log(e);
     }
+  }
+
+  async userExists() {
+    const user = await LoginModel.findOne({ email: this.body.email });
+    if (user) this.errors.push('Usuário já existe');
   }
 
   valida() {
