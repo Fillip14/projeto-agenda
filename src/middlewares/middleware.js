cdaee279282f93e307873a42798c22408daa5796
Fillip14 <fillip.apol@gmail.com>
2024-04-02 15:58:21 -0300

    Adicionado metodo para registrar usuario, verificar email e senha e exibir mensagens de erro e sucesso

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,11 +1,8 @@
 const path = require('path');
 
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.umaVariavelLocal = 'Este é o valor da var local';
-  if (req.body.cliente) {
-    console.log();
-    console.log(`Vi que voce postou ${req.body.cliente}`);
-  }
+  res.locals.errors = req.flash('errors');
+  res.locals.success = req.flash('success');
   next();
 };
 
