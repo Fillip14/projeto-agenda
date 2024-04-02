0c6174378251dc4cb933e1455632c1f50e6298dd
Fillip14 <fillip.apol@gmail.com>
2024-04-02 17:40:32 -0300

    Adicionado opcao de registrar contatos na agenda

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,3 +1,38 @@
-exports.paginaInicial = (req, res) => {
-  res.send("Obrigado por entrar em contato");
+const Contato = require('../models/contatoModel');
+
+exports.index = (req, res) => {
+  res.render('contato', {
+    contato: {},
+  });
+};
+
+exports.register = async (req, res) => {
+  try {
+    const contato = new Contato(req.body);
+    await contato.register();
+
+    if (contato.errors.length > 0) {
+      req.flash('errors', contato.errors);
+      req.session.save(() => res.redirect('/contato/index'));
+      return;
+    }
+    req.flash('success', 'Contato registrado com sucesso');
+    req.session.save(() =>
+      res.redirect(`/contato/index/${contato.contato._id}`)
+    );
+  } catch (e) {
+    console.log(e);
+    res.render('404');
+  }
+};
+
+exports.editIndex = async (req, res) => {
+  if (!req.params.id) return res.render('404');
+  const contato = new Contato(req.body);
+
+  const contatoSelect = await contato.buscaPorId(req.params.id);
+
+  if (!contatoSelect) return res.render('404');
+
+  res.render('contato', { contato: contatoSelect });
 };
